de2eeac2c2e8b92ebb6507478b2338e5e92e199d
iBoBoTi <iroegbucalistus@gmail.com>
2023-01-03 23:13:20 +0100

    ci: setup the docker compose for the authentication service binary

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -1,21 +1,38 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/iBoBoTi/microservice-arch/authentication-service/data"
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 
 const webPort = "80"
 
+var counts int64
+
 func main(){
 	log.Println("Starting Authentication Service...")
 
-	//TODO: connect to db
-	
+	// connect to db
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("Can't connect to Postgres!")
+	}
 
-	app := Config{}
+
+	app := Config{
+		DB: conn,
+		Models: data.New(conn),
+	}
 
 	// http server
 	srv := &http.Server{
@@ -25,4 +42,43 @@ func main(){
 
 	// start server
 	log.Fatal(srv.ListenAndServe())
+}
+
+func OpenDB(dsn string) (*sql.DB, error){
+	db, err :=  sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to postgres: %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("error pinging database service: %v", err)
+	}
+
+	return db, nil
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+	log.Println(dsn)
+
+	for {
+		connection, err := OpenDB(dsn)
+		if err != nil {
+			log.Println("Postgres not ready yet...")
+			counts++
+		} else {
+			log.Println("Connected to Postgres!")
+			return connection
+		}
+
+		if counts > 10{
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Backing off for two seconds")
+		time.Sleep(2 * time.Second)
+		continue
+	}
 }
--- authentication-service/go.mod
+++ authentication-service/go.mod
@@ -5,5 +5,18 @@ go 1.18
 require (
 	github.com/go-chi/chi/v5 v5.0.8
 	github.com/go-chi/cors v1.2.1
+	github.com/jackc/pgconn v1.13.0
+	github.com/jackc/pgx/v4 v4.17.2
+	github.com/joho/godotenv v1.4.0
 	golang.org/x/crypto v0.4.0
+)
+
+require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.1 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.12.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
 )
